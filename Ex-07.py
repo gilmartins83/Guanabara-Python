45bd98245e6718bdeddcf9d7b3ed2fa85d8196c0
gilmartins83 <gilmartins@adbomretiro.com.br>
2019-01-25 14:07:49 -0200

    Add files via upload

---
+++ Ex-07.py
@@ -0,0 +1,9 @@
+n1 = float(input("digite a nota 1 do aluno: "))
+n2 = float(input("digite a nota 2 do aluno: "))
+m  = (n1 + n2) / 2
+print("\n")
+
+print("a media do aluno entre {} e {} é igual a {}" .format (n1, n2, m))
+print("\n")
+
+print("A média entre {:.1f} e {:.1f} é igual a {:.1f}" . format (n1, n2, m))
