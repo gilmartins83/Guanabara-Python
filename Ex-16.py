63753f39a5ee9feb36b7c3d60c98bdeaa79d59c6
gilmartins83 <gilmartins@adbomretiro.com.br>
2019-02-09 19:33:33 -0200

    Add files via upload

---
+++ Ex-16.py
@@ -0,0 +1,7 @@
+'''from math import trunc
+num = float(input("digite um valor: "))
+print("O valor digitado foi {} e a sua porção inteira é {} " .format(num, trunc(num)))'''
+
+num  =  float(input("digite um numero: "))
+print("O valor digitado foi {} e sua parte inteira é {} " .format(num, int(num)))
+
